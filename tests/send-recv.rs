12244f947008755ef7a771b58c5ab2bf67a579bd
Philip Munksgaard <pmunksgaard@gmail.com>
2015-03-19 11:36:14 +0100

    Add a simple test
    This tests very basic functionality of `send` and `recv`.

---
+++ tests/send-recv.rs
@@ -0,0 +1,19 @@
+use std::thread::spawn;
+
+extern crate session_types;
+use session_types::*;
+
+fn client(n: u64, c: Chan<(), Send<u64, Eps>>) {
+    c.send(n).close()
+}
+
+#[test]
+fn main() {
+    let n = 42;
+    let (c1, c2) = session_channel();
+    spawn(move || client(n, c1));
+
+    let (c, n_) = c2.recv();
+    c.close();
+    assert_eq!(n, n_);
+}
